d668a8eb4afc2c8c3865e289c504e1a6339ae724
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2024-10-21 20:39:01 +0200

    Add kagglehub request to see how it behaves server-side

--- api/index.py
+++ api/index.py
@@ -3,6 +3,14 @@ from fastapi import FastAPI
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/fastapi/docs", openapi_url="/api/fastapi/openapi.json")
 
+import kagglehub
+
+
 @app.get("/api/fastapi/hello")
 def hello_fast_api():
-    return {"message": "Hello FastAPI"}
+    # Download latest version
+    path = kagglehub.dataset_download("unidatapro/crowd-counting")
+
+    print("Path to dataset files:", path)
+
+    return {"message": "Hello FastAPI" + path}
