51d5dc267be64482b1365150e5430d59cab0fe10
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2024-10-21 16:07:17 +0200

    Remove conflictive dependencies

--- api/index.py
+++ api/index.py
@@ -1,16 +1,4 @@
-from fastapi import FastAPI, Request
-
-import os
-import pickle
-from time import perf_counter as pc
-
-import nltk
-import pandas as pd
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import pairwise_distances
+from fastapi import FastAPI
 
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/fastapi/docs", openapi_url="/api/fastapi/openapi.json")
