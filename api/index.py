95a5122211f1439926ee190677f1ae6e1013cebf
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2024-10-22 17:28:56 +0200

    Reset API endpoint

--- api/index.py
+++ api/index.py
@@ -3,14 +3,11 @@ from fastapi import FastAPI
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/fastapi/docs", openapi_url="/api/fastapi/openapi.json")
 
-import kagglehub
-
 
 @app.get("/api/fastapi/hello")
 def hello_fast_api():
-    # Download latest version
-    path = kagglehub.dataset_download("unidatapro/crowd-counting")
 
-    print("Path to dataset files:", path)
+    return {"message": "Hello FastAPI"}
 
-    return {"message": "Hello FastAPI" + path}
+
+# EI1aPUIlQ6I5K8AxWfOXRszTir5JZg3R9KFbLqHScAs
