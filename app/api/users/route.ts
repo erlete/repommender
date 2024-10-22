6fe8965abb5aedf2743decffcbd0b709a68eb17b
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2024-10-22 17:29:18 +0200

    Add MongoDB support and connections

---
+++ app/api/users/route.ts
@@ -0,0 +1,80 @@
+import { NextRequest } from "next/server";
+
+import { mongoDbClient } from "@/config/db";
+
+export async function GET({ nextUrl: { searchParams } }: NextRequest) {
+  const params = {
+    username: searchParams.get("username"),
+    email: searchParams.get("email"),
+  };
+
+  if (!params.username && !params.email) {
+    return new Response("Either 'username' or 'email' must be provided", {
+      status: 400,
+    });
+  }
+
+  try {
+    await mongoDbClient.connect();
+    const usersCollection = mongoDbClient.db("recommender").collection("users");
+
+    const query = params.username
+      ? { username: params.username }
+      : { email: params.email };
+    const user = await usersCollection.findOne(query);
+
+    if (!user) {
+      return new Response("User not found", { status: 404 });
+    }
+
+    // Remove password from user object before returning it:
+    delete user.password;
+
+    return new Response(JSON.stringify(user), { status: 200 });
+  } catch (error) {
+    return new Response(`Internal error: ${error}`, { status: 500 });
+  } finally {
+    await mongoDbClient.close();
+  }
+}
+
+export async function POST(request: NextRequest) {
+  const params = await request.json();
+
+  if (!params.username || !params.email || !params.password) {
+    return new Response(
+      "Parameters 'username', 'email' and 'password' must be provided",
+      {
+        status: 400,
+      }
+    );
+  }
+
+  try {
+    await mongoDbClient.connect();
+    const usersCollection = mongoDbClient.db("recommender").collection("users");
+
+    // Check if user already exists:
+    const user = await usersCollection.findOne({
+      $or: [{ username: params.username }, { email: params.email }],
+    });
+
+    if (user) {
+      return new Response("User already exists", { status: 400 });
+    }
+
+    const newUser = {
+      username: params.username,
+      email: params.email,
+      password: params.password,
+    };
+
+    await usersCollection.insertOne(newUser);
+
+    return new Response("User created successfully", { status: 201 });
+  } catch (error) {
+    return new Response(`Internal error: ${error}`, { status: 500 });
+  } finally {
+    await mongoDbClient.close();
+  }
+}
--- config/db.ts
+++ config/db.ts
@@ -1,1 +1,15 @@
-// TODO: Add database configuration:
+import { config } from "dotenv";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+config();
+
+export const mongoDbClient = new MongoClient(
+  process.env.MONGODB_URI as string,
+  {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  }
+);
