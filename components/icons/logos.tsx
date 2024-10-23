48009e9501afb6335140758e265144373172e837
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2024-10-23 19:09:38 +0200

    Update favicon and logo color

--- components/icons/logos.tsx
+++ components/icons/logos.tsx
@@ -3,7 +3,11 @@ import { SvgWrapper } from "./utils";
 import { IconSvgProps } from "@/types";
 
 export const Logo: React.FC<IconSvgProps> = (props: IconSvgProps) => (
-  <SvgWrapper viewBox="0 0 24 24" {...props}>
+  <SvgWrapper
+    viewBox="0 0 24 24"
+    {...props}
+    className={`${props.className} fill-primary`}
+  >
     <path
       d="M3 2.75A2.75 2.75 0 015.75 0h14.5a.75.75 0 01.75.75v20.5a.75.75 0 01-.75.75h-6a.75.75 0 010-1.5h5.25v-4H6A1.5 1.5 0 004.5 18v.75c0 .716.43 1.334 1.05 1.605a.75.75 0 01-.6 1.374A3.25 3.25 0 013 18.75v-16zM19.5 1.5V15H6c-.546 0-1.059.146-1.5.401V2.75c0-.69.56-1.25 1.25-1.25H19.5z"
       fillRule="evenodd"
