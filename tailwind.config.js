f67680d7eed4cafb2bd9098a16ae6373f90e99c3
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2024-10-23 16:52:20 +0200

    Update theme palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,80 +1,124 @@
-import { nextui } from "@nextui-org/theme";
+import { colors, nextui } from "@nextui-org/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
-      },
+    content: [
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["var(--font-sans)"],
+                mono: ["var(--font-mono)"],
+            },
+        },
     },
-  },
-  darkMode: "class",
-  plugins: [nextui({
-    themes: {
-      light: {
-        colors: {
-          primary: {
-            100: "#dae6f0",
-            200: "#b5cee2",
-            300: "#8fb5d3",
-            400: "#6a9dc5",
-            500: "#4584b6",
-            600: "#376a92",
-            700: "#294f6d",
-            800: "#1c3549",
-            900: "#0e1a24",
-            DEFAULT: "#4584b6"
-          },
-          secondary: {
-            100: "#fff9df",
-            200: "#fff3be",
-            300: "#ffec9e",
-            400: "#ffe67d",
-            500: "#ffe05d",
-            600: "#ccb34a",
-            700: "#998638",
-            800: "#665a25",
-            900: "#332d13",
-            DEFAULT: "#ffe05d"
-          },
-        }
-      },
-      dark: {
-        colors: {
-          background: "#0d1117",
-          primary: {
-            100: "#dae6f0",
-            200: "#b5cee2",
-            300: "#8fb5d3",
-            400: "#6a9dc5",
-            500: "#4584b6",
-            600: "#376a92",
-            700: "#294f6d",
-            800: "#1c3549",
-            900: "#0e1a24",
-            DEFAULT: "#4584b6"
-          },
-          secondary: {
-            100: "#fff9df",
-            200: "#fff3be",
-            300: "#ffec9e",
-            400: "#ffe67d",
-            500: "#ffe05d",
-            600: "#ccb34a",
-            700: "#998638",
-            800: "#665a25",
-            900: "#332d13",
-            DEFAULT: "#ffe05d"
-          },
-        }
-      }
+    darkMode: "class",
+    plugins: [nextui({
+        themes: {
+            light: {
+                colors: {
+                    primary: {
+                        100: "#e6d9ef",
+                        200: "#cdb4e0",
+                        300: "#b58ed0",
+                        400: "#9c69c1",
+                        500: "#8343b1",
+                        600: "#69368e",
+                        700: "#4f286a",
+                        800: "#341b47",
+                        900: "#1a0d23",
+                        DEFAULT: "#8343b1",
+                    }, success: {
+                        100: "#EAFBD6",
+                        200: "#D1F7AE",
+                        300: "#ACE882",
+                        400: "#85D25E",
+                        500: "#54B530",
+                        600: "#3B9B23",
+                        700: "#268218",
+                        800: "#14680F",
+                        900: "#095609",
+                        DEFAULT: "#54B530",
+                    }, warning: {
+                        100: "#FFFBD3",
+                        200: "#FFF5A8",
+                        300: "#FFEF7C",
+                        400: "#FFE85C",
+                        500: "#FFDE26",
+                        600: "#DBBB1B",
+                        700: "#B79913",
+                        800: "#93780C",
+                        900: "#7A6107",
+                        DEFAULT: "#FFDE26",
+                    }, danger: {
+                        100: "#FFDEDA",
+                        200: "#FFB7B6",
+                        300: "#FF9199",
+                        400: "#FF768D",
+                        500: "#FF497A",
+                        600: "#DB3572",
+                        700: "#B72468",
+                        800: "#93175D",
+                        900: "#7A0E55",
+                        DEFAULT: "#FF497A",
+                    },
+                },
+            },
+            dark: {
+                colors: {
+                    background: "#0d1117",
+                    primary: {
+                        100: "#e6d9ef",
+                        200: "#cdb4e0",
+                        300: "#b58ed0",
+                        400: "#9c69c1",
+                        500: "#8343b1",
+                        600: "#69368e",
+                        700: "#4f286a",
+                        800: "#341b47",
+                        900: "#1a0d23",
+                        DEFAULT: "#8343b1",
+                    }, success: {
+                        100: "#EAFBD6",
+                        200: "#D1F7AE",
+                        300: "#ACE882",
+                        400: "#85D25E",
+                        500: "#54B530",
+                        600: "#3B9B23",
+                        700: "#268218",
+                        800: "#14680F",
+                        900: "#095609",
+                        DEFAULT: "#54B530",
+                    }, warning: {
+                        100: "#FFFBD3",
+                        200: "#FFF5A8",
+                        300: "#FFEF7C",
+                        400: "#FFE85C",
+                        500: "#FFDE26",
+                        600: "#DBBB1B",
+                        700: "#B79913",
+                        800: "#93780C",
+                        900: "#7A6107",
+                        DEFAULT: "#FFDE26",
+                    }, danger: {
+                        100: "#FFDEDA",
+                        200: "#FFB7B6",
+                        300: "#FF9199",
+                        400: "#FF768D",
+                        500: "#FF497A",
+                        600: "#DB3572",
+                        700: "#B72468",
+                        800: "#93175D",
+                        900: "#7A0E55",
+                        DEFAULT: "#FF497A",
+                    },
+                },
+            },
+        },
     }
-  })],
+    )],
 };
+
