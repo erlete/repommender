5e9b290948298e76657d3c02a89bb90eeeee1f6c
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2024-10-24 21:36:47 +0200

    Add Python version validation

--- scripts/setup.py
+++ scripts/setup.py
@@ -8,6 +8,12 @@ Author:
 """
 
 import os
+import sys
+
+# Ensure that the Python version is 3.10 or higher:
+if sys.version_info.major > 3 and sys.version_info.minor >= 10:
+    print("This script requires Python 3.10 or higher.")
+    exit(1)
 
 # Ensure that required dependencies are installed:
 try:
