af7c59914a3912493965dfbe36247329f6066455
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2025-01-07 17:52:18 +0100

    Remove redirector app

--- redirector-app/config/site.ts
+++
@@ -1,8 +0,0 @@
-export type SiteConfig = typeof siteConfig;
-
-export const siteConfig = {
-  name: "Repommender - Encuentra repositorios útiles fácilmente",
-  description:
-    "Recomendador de repositorios de GitHub basado en popularidad, contenido y colaboradores",
-  url: "https://clearly-darling-seagull.ngrok-free.app",
-};
